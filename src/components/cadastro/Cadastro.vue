54f7331dea90711201f2f50ffe850a016da8515b
sabrina-dev <sabrinassaa@hotmail.com>
2022-10-11 20:33:11 -0300

    register layout

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -1,12 +1,96 @@
+<!-- alurapic/src/components/cadastro/Cadastro.vue -->
+
 <template>
-    <h2 class="centralizado">Cadastro
-    </h2>
+
+  <div>
+    <h1 class="centralizado">Cadastro</h1>
+    <h2 class="centralizado"></h2>
+
+    <form @submit.prevent="gravar()">
+      <div class="controle">
+        <label for="titulo">TÍTULO</label>
+        <input id="titulo" autocomplete="off" @input="foto.titulo= $event.target.value" :value="foto.titulo" >
+      </div>
+
+      <div class="controle">
+        <label for="url">URL</label>
+        <input id="url" autocomplete="off" @input="foto.url= $event.target.value" :value="foto.url" >
+        <imagem-responsiva/>
+      </div>
+
+      <div class="controle">
+        <label for="descricao">DESCRIÇÃO</label>
+        <textarea id="descricao" autocomplete="off" @input="foto.descricao= $event.target.value" :value="foto.descricao" ></textarea>
+      </div>
+
+      <div class="centralizado">
+        <meu-botao rotulo="GRAVAR" tipo="submit"/>
+        <router-link to="/"><meu-botao rotulo="VOLTAR" tipo="button"/></router-link>
+      </div>
+
+    </form>
+  </div>
 </template>
+
 <script>
 
+import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
+import Botao from '../shared/botão/Botao.vue';
+
+export default {
+
+  components: {
+
+    'imagem-responsiva': ImagemResponsiva, 
+    'meu-botao': Botao
+  },
+  data(){
+
+    return {
+
+      foto: {
+        titulo:'',
+        url: '',
+        descricao: ''
+      }
+    }
+  },
+  methods: {
+  gravar() {
+   this.foto = {
+      titulo:'',
+      url: '',
+      descricao: ''
+   }
+  }}
+
+} 
+
+
 </script>
-<style>
-.centralizado{
+<style scoped>
+
+  .centralizado {
     text-align: center;
-}
+  }
+  .controle {
+    font-size: 1.2em;
+    margin-bottom: 20px;
+
+  }
+  .controle label {
+    display: block;
+    font-weight: bold;
+  }
+
+ .controle label + input, .controle textarea {
+    width: 100%;
+    font-size: inherit;
+    border-radius: 5px
+  }
+
+  .centralizado {
+    text-align: center;
+  }
+
 </style>
