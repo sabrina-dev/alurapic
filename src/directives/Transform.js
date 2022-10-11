94b027768b837219bcef17d564305e6d836fbfec
sabrina-dev <sabrinassaa@hotmail.com>
2022-10-10 23:39:33 -0300

    adding directives

--- src/directives/Transform.js
+++ src/directives/Transform.js
@@ -7,18 +7,25 @@ Vue.directive('meu-transform', {
 
       el.addEventListener('dblclick', function () {
 
-        let incremento = 90;
-        let animate = false;
-
-        if (binding.value){
-            incremento = binding.value.incremento;
-            animate = binding.value.animate;
-
+        let incremento = binding.value || 90;
+        let efeito;
+if(!binding.arg || binding.arg == 'rotate'){
+      
+    if(binding.modifiers.reverse){
+            current-= incremento;
+        }else{
+            current+=incremento;
         }
-        current+=incremento;
-     
-        el.style.transform = `rotate(${current}deg)`;
-        if(animate)    el.style.transition = 'transform 0.5s';
+        efeito = `rotate(${current}deg)`;
+   
+    }else if (binding.arg == "scale"){
+        efeito = `scale(${incremento})`;
+    }
+
+      
+        el.style.transform = efeito;
+
+        if(binding.modifiers.animate)    el.style.transition = 'transform 0.5s';
         
       });
     }
